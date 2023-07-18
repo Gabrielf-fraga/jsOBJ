9d6543345785a29ae9fb69bae39af8357a6f09ba
gabrielf-fraga <gabrielfragaQI@gmail.com>
2023-07-18 20:25:45 -0300

    curso obj js pt2

---
+++ aula2/aninhados.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "gabriel",
+    idade: 18,
+    email: "gabriel@alura.com",
+    telefone: ["88888888", "99999999"],//lista
+  };
+  
+cliente.indereco = {
+    rua: "R. bla bla bla",
+    n: 710,
+    casa: true,
+    complemeto: "casa da esquina",
+};
+console.log(cliente.indereco);
---
+++ aula2/tipos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "gabriel",
+    idade: 18,
+    email: "gabriel@alura.com",
+    telefone: ["88888888", "99999999"],//lista
+  };
+  
+console.log(cliente);
+console.log(cliente.telefone);
+  
